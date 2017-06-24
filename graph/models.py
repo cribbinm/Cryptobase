040902103ddca88b0cc4c02da6c5d884de964cfb
cribbinm <cribbinm@tcd.ie>
2017-06-24 17:19:59 +1200

    Modified templates to display prices from database.

--- graph/models.py
+++ graph/models.py
@@ -3,6 +3,11 @@ from django.utils import timezone
 
 
 class Price(models.Model):
-    datetime = models.DateTimeField(default=timezone.now)
-    xbt = models.DecimalField(max_digits=15, decimal_places=9)
-    eth = models.DecimalField(max_digits=15, decimal_places=9)
+    published_date = models.DateTimeField(default=timezone.now)
+    # xbt = models.DecimalField(max_digits=15, decimal_places=9)
+    # eth = models.DecimalField(max_digits=15, decimal_places=9)
+    xbt = models.CharField(max_length=200)
+    eth = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.xbt
--- graph/views.py
+++ graph/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from .models import Price
+from django.utils import timezone
 # Create your views here.
 
 def price_list(request):
-    return render(request, 'graph/price_list.html', {})
+    prices = Price.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'graph/price_list.html', {'prices': prices})
